2b446fda9f3be8d0e2a4c3cb81dc714418ef5121
Agustín <aguformoso@gmail.com>
2016-08-31 14:22:00 +0200

    no message

--- charts/charts/settings.py
+++ charts/charts/settings.py
@@ -57,6 +57,43 @@ ROOT_URLCONF = 'charts.urls'
 
 WSGI_APPLICATION = 'charts.wsgi.application'
 
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    # 'filters': {
+    # 'require_debug_false': {
+    # '()': 'django.utils.log.RequireDebugFalse'
+    #        }
+    #    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            # 'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
@@ -86,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DEBUG = DEBUG
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
--- charts/charts/urls.py
+++ charts/charts/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-                       url(r'^charts/', include('app.urls')),
-
-                       url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^charts/', include('app.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+]
