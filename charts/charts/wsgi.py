8f3df2aac2c075de35950432dd3fac7c6b101acf
Agustín <aguformoso@gmail.com>
2016-09-08 12:04:14 +0200

    WSGI file fix

--- charts/charts/wsgi.py
+++ charts/charts/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os, sys
 
 sys.path.append('/opt/django/lacnic-charts/charts')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charts.settings")
+sys.path.append('/Users/agustin/git/lacnic-charts/charts')
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charts.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'charts.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
