959b5c5bb7335ac68aa57463a53fa9c496fef2c7
Agustín <aguformoso@gmail.com>
2016-09-08 12:32:53 +0200

    no message

--- charts/charts/settings.py
+++ charts/charts/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import os, socket
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
