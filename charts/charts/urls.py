db0e6143034d40540557836f848201ec5979d9ae
Agustín <aguformoso@gmail.com>
2016-09-08 12:15:51 +0200

    no message

--- charts/charts/urls.py
+++ charts/charts/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^charts/', include('app.urls')),
+    url(r'^/', include('app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
