f22a45a18d2fe47903b0b1279b68116f8172a75e
Agustín <aguformoso@gmail.com>
2016-09-08 12:19:51 +0200

    no message

--- charts/app/urls.py
+++ charts/app/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-                       url(r'^$', 'app.views.home', name='home'),
-                       url(r'^code$', 'app.views.code', name='code'),
+                       url(r'^/$', 'app.views.home', name='home'),
+                       url(r'^code/$', 'app.views.code', name='code'),
 
-                       url(r'^hist$', 'app.views.hist', name='hist'),
-                       url(r'^hist/code$', 'app.views.code_hist', name='hist'),
+                       url(r'^hist/$', 'app.views.hist', name='hist'),
+                       url(r'^hist/code/$', 'app.views.code_hist', name='hist'),
 )
