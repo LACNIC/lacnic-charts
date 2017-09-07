724af2b82a6994fac5b907cc495f97a27ce19dc0
Agustín <aguformoso@gmail.com>
2017-09-07 13:56:20 -0300

    POST test case

--- charts/app/tests/tests.py
+++ charts/app/tests/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase, Client
 
 
 class ChartTestCases(TestCase):
-
     def test_con_x_y(self):
         c = Client()
         r = c.get("/?x=[1,2,3,4,5]&ys=[[5,4,3,3,3]]&labels=[%271%20eje%20ys%27]&divId=chart")
@@ -15,32 +14,38 @@ class ChartTestCases(TestCase):
 
     def test_con_x_2ys(self):
         c = Client()
-        r = c.get("?x=[1,2,3,4,5]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27dos%20ejes%20ys%27,%20%27mi%20otra%20label%27]&divId=chart")
+        r = c.get(
+            "?x=[1,2,3,4,5]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27dos%20ejes%20ys%27,%20%27mi%20otra%20label%27]&divId=chart")
         self.assertEqual(r.status_code, 200)
 
     def test_con_x_2ys_Stacked(self):
         c = Client()
-        r = c.get("/?x=[1,2,3,4,5]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27dos%20ejes%20ys%20stacked%27,%20%27mi%20otra%20label%27]&divId=chart&stacked=true")
+        r = c.get(
+            "/?x=[1,2,3,4,5]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27dos%20ejes%20ys%20stacked%27,%20%27mi%20otra%20label%27]&divId=chart&stacked=true")
         self.assertEqual(r.status_code, 200)
 
     def test_con_fechas_x(self):
         c = Client()
-        r = c.get("/?x=[%272017-06-15%27,%272017-06-16%27,%272017-06-17%27,%272017-06-18%27,%272017-06-19%27]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27eje%20x%20con%20fecha%27,%20%27mi%20otra%20label%27]&divId=chart&xType=%27date%27&stacked=%27true%27&colors=[#FAA519,%27blue%27]")
+        r = c.get(
+            "/?x=[%272017-06-15%27,%272017-06-16%27,%272017-06-17%27,%272017-06-18%27,%272017-06-19%27]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27eje%20x%20con%20fecha%27,%20%27mi%20otra%20label%27]&divId=chart&xType=%27date%27&stacked=%27true%27&colors=[#FAA519,%27blue%27]")
         self.assertEqual(r.status_code, 200)
 
     def test_con_y_ys(self):
         c = Client()
-        r = c.get("/?x=[%272017-06-15%27,%272017-06-16%27,%272017-06-17%27,%272017-06-18%27,%272017-06-19%27]&y=[3,3,3,3,3]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27Mi%20label%27,%27con%20una%20y%20y%20una%20ys.%20debe%20ignorar%20la%20y%27,%27el%20tercero%27]&divId=chart&xType=%27date%27&stacked=%27true%27&colors=[#FAA519,%27blue%27]")
+        r = c.get(
+            "/?x=[%272017-06-15%27,%272017-06-16%27,%272017-06-17%27,%272017-06-18%27,%272017-06-19%27]&y=[3,3,3,3,3]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27Mi%20label%27,%27con%20una%20y%20y%20una%20ys.%20debe%20ignorar%20la%20y%27,%27el%20tercero%27]&divId=chart&xType=%27date%27&stacked=%27true%27&colors=[#FAA519,%27blue%27]")
         self.assertEqual(r.status_code, 200)
 
     def test_area_char_con_fecha(self):
         c = Client()
-        r = c.get("/?x=[%272017-06-15%27,%272017-06-16%27,%272017-06-17%27,%272017-06-18%27,%272017-06-19%27]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27prueba%20area%20chart%20con%20fecha%27,%27label%27]&divId=chart&xType=%27date%27&kind=AreaChart")
+        r = c.get(
+            "/?x=[%272017-06-15%27,%272017-06-16%27,%272017-06-17%27,%272017-06-18%27,%272017-06-19%27]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27prueba%20area%20chart%20con%20fecha%27,%27label%27]&divId=chart&xType=%27date%27&kind=AreaChart")
         self.assertEqual(r.status_code, 200)
 
     def test_area_char_con_fecha_stacked(self):
         c = Client()
-        r = c.get("/?x=[%272017-06-15%27,%272017-06-16%27,%272017-06-17%27,%272017-06-18%27,%272017-06-19%27]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27prueba%20area%20chart%20con%20fecha%20STACKED%27,%27label%27]&divId=chart&xType=%27date%27&kind=AreaChart&stacked=true")
+        r = c.get(
+            "/?x=[%272017-06-15%27,%272017-06-16%27,%272017-06-17%27,%272017-06-18%27,%272017-06-19%27]&ys=[[5,4,3,3,3],[1,2,3,4,5]]&labels=[%27prueba%20area%20chart%20con%20fecha%20STACKED%27,%27label%27]&divId=chart&xType=%27date%27&kind=AreaChart&stacked=true")
         self.assertEqual(r.status_code, 200)
 
     def test_hist_simple(self):
@@ -58,4 +63,16 @@ class ChartTestCases(TestCase):
         r = c.get("/?x=[15,10]&xType=string&labels=[%27A%20-%20label%27,%27B%20-%20label%27]&kind=PieChart")
         self.assertEqual(r.status_code, 200)
 
-
+    def test_pie_chart_post(self):
+        c = Client()
+        r = c.post(
+            path="/code/",
+            data={
+                'x': '[15,10]',
+                'xType': 'string',
+                'labels': "['A-label', 'B-label']",
+                'kind': 'PieChart',
+                'divId': 'mi_div_id'
+            }
+        )
+        self.assertEqual(r.status_code, 200)
